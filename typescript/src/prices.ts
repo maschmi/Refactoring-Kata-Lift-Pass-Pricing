1d730651f6bdd6bf6233cf5ea178e673b72db100
Martin Schmidt <martin.schmidt@qualityminds.de>
2023-09-19 07:36:51 +0200

    removes legacy night pass code

--- typescript/src/prices.ts
+++ typescript/src/prices.ts
@@ -26,51 +26,40 @@ export function calcTicketPrice(age: number, type: string, date: string | undefi
     if (ticket !== undefined) {
         return ticket.withBasePrice(basePrice);
     }
-    if (type === 'night') {
-        if (age >= 6) {
-            if (age > 64) {
-                return ({cost: Math.ceil(basePrice.cost * .4)})
-            } else {
-                return (basePrice)
+
+    let isHoliday;
+    let reduction = 0
+    for (let holiday of holidays) {
+        if (date) {
+            let d = new Date(date as string)
+            if (d.getFullYear() === holiday.getFullYear()
+                && d.getMonth() === holiday.getMonth()
+                && d.getDate() === holiday.getDate()) {
+
+                isHoliday = true
             }
-        } else {
-            return (zeroBasePrice)
         }
+
+    }
+
+    if (!isHoliday && new Date(date as string).getDay() === 1) {
+        reduction = 35
+    }
+
+    // TODO apply reduction for others
+    if (age < 15) {
+        return ({cost: Math.ceil(basePrice.cost * .7)})
     } else {
-        let isHoliday;
-        let reduction = 0
-        for (let holiday of holidays) {
-            if (date) {
-                let d = new Date(date as string)
-                if (d.getFullYear() === holiday.getFullYear()
-                    && d.getMonth() === holiday.getMonth()
-                    && d.getDate() === holiday.getDate()) {
-
-                    isHoliday = true
-                }
-            }
-
-        }
-
-        if (!isHoliday && new Date(date as string).getDay() === 1) {
-            reduction = 35
-        }
-
-        // TODO apply reduction for others
-        if (age < 15) {
-            return ({cost: Math.ceil(basePrice.cost * .7)})
+        if (age === undefined) {
+            let cost = basePrice.cost * (1 - reduction / 100)
+            return ({cost: Math.ceil(cost)})
         } else {
-            if (age === undefined) {
-                let cost = basePrice.cost * (1 - reduction / 100)
+            if (age > 64) {
+                let cost = basePrice.cost * .75 * (1 - reduction / 100)
                 return ({cost: Math.ceil(cost)})
             } else {
-                if (age > 64) {
-                    let cost = basePrice.cost * .75 * (1 - reduction / 100)
-                    return ({cost: Math.ceil(cost)})
-                } else {
-                    let cost = basePrice.cost * (1 - reduction / 100)
-                    return ({cost: Math.ceil(cost)})
-                }
+                let cost = basePrice.cost * (1 - reduction / 100)
+                return ({cost: Math.ceil(cost)})
             }
         }
     }

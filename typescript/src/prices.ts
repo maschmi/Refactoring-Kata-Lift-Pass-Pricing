0ddc8feb499ae895894b02540fd94934411fe2ef
Martin Schmidt <martin.schmidt@qualityminds.de>
2023-09-21 13:48:42 +0200

    extracted logic

--- typescript/src/prices.ts
+++ typescript/src/prices.ts
@@ -15,6 +15,63 @@ async function someHolidays(connection: Connection) {
     return holidays;
 }
 
+function logicForRealThisTime(age: string | undefined, type, holidays: RowDataPacket[], date: string | undefined, result) {
+    if (age as any < 6) {
+        return ({cost: 0})
+    } else {
+        if (type !== 'night') {
+
+            let isHoliday;
+            let reduction = 0
+            for (let row of holidays) {
+                let holiday = row.holiday
+                if (date) {
+                    let d = new Date(date as string)
+                    if (d.getFullYear() === holiday.getFullYear()
+                        && d.getMonth() === holiday.getMonth()
+                        && d.getDate() === holiday.getDate()) {
+
+                        isHoliday = true
+                    }
+                }
+
+            }
+
+            if (!isHoliday && new Date(date as string).getDay() === 1) {
+                reduction = 35
+            }
+
+            // TODO apply reduction for others
+            if (age as any < 15) {
+                return ({cost: Math.ceil(result.cost * .7)})
+            } else {
+                if (age === undefined) {
+                    let cost = result.cost * (1 - reduction / 100)
+                    return ({cost: Math.ceil(cost)})
+                } else {
+                    if (age as any > 64) {
+                        let cost = result.cost * .75 * (1 - reduction / 100)
+                        return ({cost: Math.ceil(cost)})
+                    } else {
+                        let cost = result.cost * (1 - reduction / 100)
+                        return ({cost: Math.ceil(cost)})
+                    }
+                }
+            }
+        } else {
+            if (age as any >= 6) {
+                if (age as any > 64) {
+                    return ({cost: Math.ceil(result.cost * .4)})
+                } else {
+                    return (result)
+                }
+            } else {
+                return ({cost: 0})
+            }
+        }
+    }
+}
+
 async function createApp() {
     const app = express()
 
@@ -29,7 +86,7 @@ async function createApp() {
             'ON DUPLICATE KEY UPDATE cost = ?',
             [liftPassType, liftPassCost, liftPassCost]);
 
-        res.json()
+        response=()
     })
     app.get('/prices', async (req, res) => {
         const result = await getSomeResult(connection, req)
@@ -39,60 +96,8 @@ async function createApp() {
         const type = req.query.type;
         const date = req.query.date;
         let response: any;
-        if (age as any < 6) {
-            res.json({cost: 0})
-        } else {
-            if (type !== 'night') {
-
-                let isHoliday;
-                let reduction = 0
-                for (let row of holidays) {
-                    let holiday = row.holiday
-                    if (date) {
-                        let d = new Date(date as string)
-                        if (d.getFullYear() === holiday.getFullYear()
-                            && d.getMonth() === holiday.getMonth()
-                            && d.getDate() === holiday.getDate()) {
-
-                            isHoliday = true
-                        }
-                    }
-
-                }
-
-                if (!isHoliday && new Date(date as string).getDay() === 1) {
-                    reduction = 35
-                }
-
-                // TODO apply reduction for others
-                if (age as any < 15) {
-                    res.json({cost: Math.ceil(result.cost * .7)})
-                } else {
-                    if (age === undefined) {
-                        let cost = result.cost * (1 - reduction / 100)
-                        res.json({cost: Math.ceil(cost)})
-                    } else {
-                        if (age as any > 64) {
-                            let cost = result.cost * .75 * (1 - reduction / 100)
-                            res.json({cost: Math.ceil(cost)})
-                        } else {
-                            let cost = result.cost * (1 - reduction / 100)
-                            res.json({cost: Math.ceil(cost)})
-                        }
-                    }
-                }
-            } else {
-                if (age as any >= 6) {
-                    if (age as any > 64) {
-                        res.json({cost: Math.ceil(result.cost * .4)})
-                    } else {
-                        res.json(result)
-                    }
-                } else {
-                    res.json({cost: 0})
-                }
-            }
-        }
+        response = logicForRealThisTime(age, type, holidays, date, result);
+        res.json(response);
     })
     return {app, connection}
 }
